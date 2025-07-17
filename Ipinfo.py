6b87c0cf92d359bdfad68097906bfb1ef6650fbb
Sumedha Das <sumedhadas45@gmail.com>
2025-07-17 10:51:27 +0530

    Add files via upload

---
+++ Ipinfo.py
@@ -0,0 +1,12 @@
+#Python Code using ipinfo.io
+
+import requests
+
+response = requests.get("https://ipinfo.io")
+data = response.json()
+
+print("IP:", data.get("ip"))
+print("City:", data.get("city"))
+print("Region:", data.get("region"))
+print("Country:", data.get("country"))
+print("Location (lat,long):", data.get("loc"))
